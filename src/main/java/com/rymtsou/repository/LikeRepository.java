68235b1439614267bde0de9c1b217600184ed610
Rymtsov Ilya <rimtsov.ilya@gmail.com>
2025-05-30 17:16:19 +0300

    Annotated LikeRepository.

--- src/main/java/com/rymtsou/repository/LikeRepository.java
+++ src/main/java/com/rymtsou/repository/LikeRepository.java
@@ -5,9 +5,11 @@ import com.rymtsou.model.domain.Like;
 import com.rymtsou.model.domain.Post;
 import com.rymtsou.model.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface LikeRepository extends JpaRepository<Like, Long> {
     Optional<Like> findByUserAndPost(User user, Post post);
     Optional<Like> findByUserAndComment(User user, Comment comment);
